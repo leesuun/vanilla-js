c5915d5d1d1e2005fc6fc5031084df1c533117e6
leesuun <77526673+leesuun@users.noreply.github.com>
2021-11-23 15:40:08 +0900

    endTalk

---
+++ webGame/endTalk/index.js
@@ -0,0 +1,52 @@
+const numberForm = document.getElementById("numberForm");
+const gameForm = document.getElementById("gameForm");
+const personNumber = document.getElementById("personNumber");
+const order = document.getElementById("order");
+
+let number;
+let word;
+let newWord = [];
+let personCnt = 1;
+
+gameForm.hidden = true;
+
+function getNumberOfPeople(event) {
+    event.preventDefault();
+    const input = numberForm.querySelector("input");
+    number = input.value;
+
+    personNumber.innerText = `${personCnt}번`;
+    numberForm.hidden = true;
+    gameForm.hidden = false;
+}
+
+function handleGame(event) {
+    personCnt += 1;
+    if (personCnt === Number(number) + 1) {
+        personCnt = 1;
+    }
+
+    personNumber.innerText = `${personCnt}번`;
+
+    event.preventDefault();
+    const input = gameForm.querySelector("input");
+    word = input.value;
+    newWord.push(input.value);
+
+    if (newWord.length >= 2) {
+        const preWord = newWord[newWord.length - 2];
+        const nextWord = newWord[newWord.length - 1];
+        if (
+            word.length !== 3 ||
+            !word ||
+            preWord[preWord.length - 1] !== nextWord[0]
+        ) {
+            alert(`${personCnt - 1}번 탈락!!`);
+        } else {
+            order.innerText = `제시어: ${word}`;
+        }
+    }
+}
+
+numberForm.addEventListener("submit", getNumberOfPeople);
+gameForm.addEventListener("submit", handleGame);
