1ada03a694a449f96c7a1054088d98d3527e162f
leesuun <77526673+leesuun@users.noreply.github.com>
2021-11-29 19:27:38 +0900

    calculator-self

--- webGame/calculator/index.js
+++ webGame/calculator/index.js
@@ -1,79 +1,158 @@
 "use strict";
 
-const btnBox = document.getElementById("btnBox");
 const screen = document.getElementById("screen");
-const buttons = btnBox.querySelectorAll("button");
+
+const one = document
+    .getElementById("num-one")
+    .addEventListener("click", handleClickNumber);
+const two = document
+    .getElementById("num-two")
+    .addEventListener("click", handleClickNumber);
+const three = document
+    .getElementById("num-three")
+    .addEventListener("click", handleClickNumber);
+const four = document
+    .getElementById("num-four")
+    .addEventListener("click", handleClickNumber);
+const five = document
+    .getElementById("num-five")
+    .addEventListener("click", handleClickNumber);
+const six = document
+    .getElementById("num-six")
+    .addEventListener("click", handleClickNumber);
+const seven = document
+    .getElementById("num-seven")
+    .addEventListener("click", handleClickNumber);
+const eight = document
+    .getElementById("num-eight")
+    .addEventListener("click", handleClickNumber);
+const nine = document
+    .getElementById("num-nine")
+    .addEventListener("click", handleClickNumber);
+const zero = document
+    .getElementById("num-zero")
+    .addEventListener("click", handleClickNumber);
+
+const division = document
+    .getElementById("division")
+    .addEventListener("click", handleClickOperator);
+const multiply = document
+    .getElementById("multiply")
+    .addEventListener("click", handleClickOperator);
+const subtract = document
+    .getElementById("subtract")
+    .addEventListener("click", handleClickOperator);
+const plus = document
+    .getElementById("plus")
+    .addEventListener("click", handleClickOperator);
+
+const clear = document
+    .getElementById("clear")
+    .addEventListener("click", handleClickClear);
+const result = document
+    .getElementById("result")
+    .addEventListener("click", handleClickResult);
 
 let firstNum = "";
-let secondNum = "";
+let nextNum = "";
+let thirdNum = "";
 let operator = "";
-let result = "";
+let nextOperator = "";
 
-const span = document.createElement("span");
-function handleBtnClick(event) {
-    const { innerText } = event.target;
-    const regexNumber = /([0-9])/;
-    const regexOper = /([-+*/])/;
-    const regexCalc = /([=])/;
-    const regexClear = /([C])/;
-
-    // 숫자 대입
-    if (operator === "") {
-        if (regexNumber.test(innerText)) {
-            firstNum += innerText;
+function showScreen(firstNum, operator, nextNum) {
+    screen.innerText = `${firstNum} ${operator} ${nextNum} ${nextOperator} ${thirdNum}`;
+}
+function handleClickNumber() {
+    if (!nextOperator) {
+        if (!operator) {
+            firstNum += this.textContent;
+        } else {
+            nextNum += this.textContent;
         }
     } else {
-        if (regexNumber.test(innerText)) {
-            secondNum += innerText;
-        }
+        thirdNum += this.textContent;
     }
 
-    // 연산 선택
-    if (regexOper.test(innerText)) {
-        operator = innerText;
-    }
-
-    // 초기화
-    if (regexClear.test(innerText)) {
-        firstNum = "";
-        secondNum = "";
-        operator = "";
-        result = "";
-
-        screen.removeChild(span);
-    }
-
-    // 계산
-    if (regexCalc.test(innerText)) {
-        switch (operator) {
-            case "+": {
-                result = Number(firstNum) + Number(secondNum);
-                break;
-            }
-            case "-": {
-                result = Number(firstNum) - Number(secondNum);
-                break;
-            }
-            case "*": {
-                result = Number(firstNum) * Number(secondNum);
-                break;
-            }
-            case "/": {
-                result = Number(firstNum) / Number(secondNum);
-                break;
-            }
-            default: {
-                break;
-            }
-        }
-
-        if (firstNum && operator && secondNum) {
-            span.innerText = `${firstNum} ${operator} ${secondNum} = ${result} `;
-            screen.appendChild(span);
-        }
-    }
+    showScreen(firstNum, operator, nextNum, nextOperator, thirdNum);
 }
 
-buttons.forEach((btn) => {
-    btn.addEventListener("click", handleBtnClick);
-});
+function handleClickOperator() {
+    if (!operator) {
+        operator = this.textContent;
+    } else {
+        nextOperator = this.textContent;
+    }
+
+    showScreen(firstNum, operator, nextNum, nextOperator, thirdNum);
+}
+
+function handleClickClear() {
+    firstNum = "";
+    nextNum = "";
+    operator = "";
+    showScreen(firstNum, operator, nextNum, nextOperator, thirdNum);
+}
+
+function handleClickResult() {
+    let result;
+    switch (operator) {
+        case "+": {
+            result = add(firstNum, nextNum);
+            break;
+        }
+        case "-": {
+            result = sub(firstNum, nextNum);
+            break;
+        }
+        case "*": {
+            result = mul(firstNum, nextNum);
+            break;
+        }
+        case "/": {
+            result = div(firstNum, nextNum);
+            break;
+        }
+        default: {
+            break;
+        }
+    }
+    switch (nextOperator) {
+        case "+": {
+            result = add(result, thirdNum);
+            break;
+        }
+        case "-": {
+            result = sub(result, thirdNum);
+            break;
+        }
+        case "*": {
+            result = mul(result, thirdNum);
+            break;
+        }
+        case "/": {
+            result = div(result, thirdNum);
+            break;
+        }
+        default: {
+            break;
+        }
+    }
+
+    screen.innerText = `결과값: ${result}`;
+}
+
+function add(firstNum, nextNum) {
+    return Number(firstNum) + Number(nextNum);
+}
+function sub(firstNum, nextNum) {
+    return firstNum - nextNum;
+}
+function mul(firstNum, nextNum) {
+    return firstNum * nextNum;
+}
+function div(firstNum, nextNum) {
+    if (Number(nextNum) === 0) {
+        return "Can not division 0";
+    }
+    return firstNum / nextNum;
+}
--- webGame/endTalk/index.js
+++ webGame/endTalk/index.js
@@ -4,48 +4,61 @@ const personNumber = document.getElementById("personNumber");
 const order = document.getElementById("order");
 
 let number;
-let word;
 let newWord = [];
 let personCnt = 1;
+let gameCnt = 0;
 
 gameForm.hidden = true;
 
 function getNumberOfPeople(event) {
     event.preventDefault();
     const input = numberForm.querySelector("input");
-    number = input.value;
 
+    if (input.value < 2) {
+        alert("2명 이상만 플레이 할 수 있습니다.");
+        return;
+    }
+
+    number = input.value;
     personNumber.innerText = `${personCnt}번`;
     numberForm.hidden = true;
     gameForm.hidden = false;
 }
 
 function handleGame(event) {
+    event.preventDefault();
+    gameCnt++;
+
+    const input = gameForm.querySelector("input");
+    const WORD_LENGTH_LIMIT = 3;
+    let word = input.value;
+
+    if (input.value.length !== WORD_LENGTH_LIMIT && gameCnt === 1) {
+        alert("첫 제시어는 3글자여야 합니다.");
+        return;
+    }
+
     personCnt += 1;
     if (personCnt === Number(number) + 1) {
         personCnt = 1;
     }
 
     personNumber.innerText = `${personCnt}번`;
-
-    event.preventDefault();
-    const input = gameForm.querySelector("input");
-    word = input.value;
     newWord.push(input.value);
     order.innerText = `제시어: ${word}`;
 
-    if (newWord.length >= 1) {
+    if (newWord.length > 1) {
         const preWord = newWord[newWord.length - 2];
         const nextWord = newWord[newWord.length - 1];
         if (
-            word.length !== 3 ||
+            word.length !== WORD_LENGTH_LIMIT ||
             !word ||
             preWord[preWord.length - 1] !== nextWord[0]
         ) {
             alert(`${personCnt - 1}번 탈락!!`);
-        } else {
-            order.innerText = `제시어: ${word}`;
+            return;
         }
+        order.innerText = `제시어: ${word}`;
     }
 }
 
