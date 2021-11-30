9efb1dbd33c82956cffabc1051483f68173cf0d3
leesuun <77526673+leesuun@users.noreply.github.com>
2021-12-01 00:15:27 +0900

    upgrade random number

--- webGame/numbers-baseball/index.js
+++ webGame/numbers-baseball/index.js
@@ -1,32 +1,37 @@
-function randomNumber() {
-    const MIN = 1000;
-    const MAX = 10000;
-    let number;
-    let numberCheck = 0;
+// 처음 짠 랜덤 4자리 코드
+const MIN = 1000;
+const MAX = 10000;
+let number;
+let numberCheck = 0;
 
-    do {
-        number = Math.ceil(Math.random() * (MAX - MIN) + MIN)
-            .toString()
-            .split("");
+let cnt = 0;
 
-        for (let i = 0; i < number.length; i++) {
-            for (let j = i + 1; j < number.length; j++) {
-                if (number[i] === number[j]) {
-                    numberCheck = 1;
-                } else {
-                    numberCheck = 0;
-                }
+do {
+    cnt++;
+    numberCheck = 0;
+    number = Math.ceil(Math.random() * (MAX - MIN) + MIN)
+        .toString()
+        .split("");
+
+    console.log(...new Set(number), cnt);
+
+    for (let i = 0; i < number.length; i++) {
+        for (let j = i + 1; j < number.length; j++) {
+            if (number[i] === number[j]) {
+                numberCheck = 1;
             }
         }
-    } while (numberCheck > 0);
+    }
+} while (numberCheck === 1);
 
-    return number;
-}
+console.log(number);
+
+// ssssssssssssssssssssssssssssssssssssssssssssssssssss
 
 let list = [];
 while (true) {
     let n = ~~(Math.random() * 10); // 0~9 숫자하나 랜덤으로 얻음
-    (list.length || n) && list.push(n); // list요소가 하나라도 있거나 n이 0보다 크면 list에 추가
+    (list.length || !n) && list.push(n); // list요소가 하나라도 있거나 n이 0보다 크면 list에 추가
     list = [...new Set(list)]; // 중복값제거
     if (list.length >= 4) break; // 중복값제거하고 나서 요소 수가 4개면 반복 종료
 }
