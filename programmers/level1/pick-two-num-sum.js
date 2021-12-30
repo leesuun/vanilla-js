cd3b721453235aab50d65173e47bb2f674423889
leesuun <77526673+leesuun@users.noreply.github.com>
2021-12-30 17:01:33 +0900

    s

--- programmers/level1/pick-two-num-sum.js
+++ programmers/level1/pick-two-num-sum.js
@@ -2,6 +2,7 @@
 
 function solution(numbers) {
     var answer = [];
+
     for (let i = 0; i < numbers.length; i++) {
         for (let j = i + 1; j < numbers.length; j++) {
             answer.push(numbers[i] + numbers[j]);
