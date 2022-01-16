8e384745390b55508f2e66ac2412cc4b2b136884
leesuun <77526673+leesuun@users.noreply.github.com>
2022-01-16 13:48:12 +0900

    backjeon

---
+++ backjeon/15552.js
@@ -0,0 +1,10 @@
+let input = require("fs").readFileSync("../input.txt").toString().split("\n");
+let count = Number(input[0]);
+let answerStr = "";
+
+for (let i = 1; i <= count; i++) {
+    let num = input[i].split(" ");
+    answerStr += Number(num[0]) + Number(num[1]) + "\n";
+}
+
+console.log(answerStr);
