d6064731f30d682b3cbc77a0db55e0af4c988841
SuUn Lee <77526673+leesuun@users.noreply.github.com>
2023-01-09 23:39:40 +0900

    feat: 피보나치의 수

---
+++ programmers/level2/피보나치수.js
@@ -0,0 +1,13 @@
+function solution(n) {
+  let previous = 0;
+  let next = 1;
+  let current = 1;
+
+  for (let i = 1; i <= n - 1; i++) {
+    current += previous;
+    previous = next;
+    next = current;
+  }
+
+  return current % 1234567;
+}
