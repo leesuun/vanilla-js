01c7d2ad1696009137796f79c80c024150fafd7c
SuUn Lee <77526673+leesuun@users.noreply.github.com>
2023-01-11 00:13:54 +0900

    feat: 프로그래머스 Lv2: 다음 큰 숫자 수정

--- programmers/level2/다음 큰 숫자.js
+++ programmers/level2/다음 큰 숫자.js
@@ -1,33 +1,25 @@
 function solution(n) {
-  var answer = 0;
-  let quotient = null;
-  let remainder = null;
-  let N = n;
-  let oneCount = 0;
-  let nextCount = 0;
+  let answer = 0;
+  const count1 = convertBinary(n);
 
-  while (quotient !== 0) {
-    quotient = Math.floor(N / 2);
-    remainder = N % 2;
-    N = quotient;
-    if (remainder === 1) oneCount++;
-  }
-  quotient = null;
-  for (let i = n + 1; i < 1000000; i++) {
-    let N = i;
-    while (quotient !== 0) {
-      quotient = Math.floor(N / 2);
-      remainder = N % 2;
-      N = quotient;
-
-      if (remainder === 1) nextCount++;
-      console.log(nextCount);
-      if (nextCount + "" === oneCount + "") {
-        answer = i;
-        return;
-      }
+  for (let i = n + 1; i <= 1000000; i++) {
+    const count2 = convertBinary(i);
+    if (count2 === count1) {
+      answer = i;
+      break;
     }
   }
-
   return answer;
+}
+
+function convertBinary(n) {
+  let quotient = n;
+  let remainder = 0;
+  let count = 0;
+  while (quotient !== 0) {
+    remainder = quotient % 2;
+    quotient = Math.floor(quotient / 2);
+    if (remainder === 1) count++;
+  }
+  return count;
 }
