108f7d8352b25c0f33a5e9519220eb830e2fccf8
SuUn Lee <77526673+leesuun@users.noreply.github.com>
2023-01-21 22:59:32 +0900

    feat: 프로그래머스 Lv2 예상대진표

--- programmers/level2/예상대진표.js
+++ programmers/level2/예상대진표.js
@@ -1,20 +1,27 @@
 function solution(n, a, b) {
   let answer = 0;
-  const order = new Array(n)
+  let order = new Array(n)
     .fill(0)
     .map((_, index) => (index + 1 === a || index + 1 === b ? 1 : 0));
 
-  const temp = [];
+  let temp = [];
 
-  for (let i = 0; i < order.length; i += 2) {
-    if ((order[i] || order[i + 1]) === 0) {
-      temp.push(0);
-    } else {
-      temp.push(1);
+  while (order.length !== 1) {
+    for (let i = 0; i < order.length; i += 2) {
+      if ((order[i] || order[i + 1]) === 0) {
+        temp.push(0);
+      } else if (order[i] === 1 && order[i + 1] === 1) {
+        answer++;
+        return answer;
+      } else {
+        temp.push(1);
+      }
     }
-  }
 
-  console.log(temp);
+    order = temp;
+    temp = [];
+    answer++;
+  }
 
   return answer;
 }
