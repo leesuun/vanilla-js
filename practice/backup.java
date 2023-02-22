03013cbd1cfdc58e7138141178f48713a6803c2a
SuUn Lee <77526673+leesuun@users.noreply.github.com>
2023-02-22 21:03:49 +0900

    db

--- practice/backup.java
+++ practice/backup.java
@@ -1,157 +1,158 @@
-package phone;
 
-import java.util.Scanner;
+// package phone;
 
-class Phone {
+// import java.util.Scanner;
 
-	private String name;
-	private String tel;
+// class Phone {
 
-	public Phone(String name, String tel) {
-		this.name = name;
-		this.tel = tel;
-	}
+// 	private String name;
+// 	private String tel;
 
-	public String getName() {
-		return name;
-	}
+// 	public Phone(String name, String tel) {
+// 		this.name = name;
+// 		this.tel = tel;
+// 	}
 
-	public String getTel() {
-		return tel;
-	}
-}
+// 	public String getName() {
+// 		return name;
+// 	}
 
-public class PhoneBook {
+// 	public String getTel() {
+// 		return tel;
+// 	}
+// }
 
-	private final String SEARCH = "검색";
-	private final String SHOWLIST = "전체검색";
-	private final String EDIT = "수정";
-	private final String DELETE = "삭제";
-	private final String EXIT = "종료";
+// public class PhoneBook {
 
-	private Scanner sc = new Scanner(System.in);
-	private Phone[] phoneAry;
+// 	private final String SEARCH = "검색";
+// 	private final String SHOWLIST = "전체검색";
+// 	private final String EDIT = "수정";
+// 	private final String DELETE = "삭제";
+// 	private final String EXIT = "종료";
 
-	private void initPhoneBook() {
-		System.out.printf("인원수:");
-		try {
-			phoneAry = new Phone[sc.nextInt()];
-		} catch (Exception e) {
-			// TODO: handle exception
-			System.out.println("숫자만 입력해주세요...");
-		}
+// 	private Scanner sc = new Scanner(System.in);
+// 	private Phone[] phoneAry;
 
-		for (int i = 0; i < phoneAry.length; i++) {
-			System.out.printf("이름과 번호입력:");
-			String name = sc.next(), tel = sc.next();
-			phoneAry[i] = new Phone(name, tel);
-		}
-		System.out.println("저장되었습니다...");
-		System.out.println();
-	}
+// 	private void initPhoneBook() {
+// 		System.out.printf("인원수:");
+// 		try {
+// 			phoneAry = new Phone[sc.nextInt()];
+// 		} catch (Exception e) {
+// 			// TODO: handle exception
+// 			System.out.println("숫자만 입력해주세요...");
+// 		}
 
-	/*
-	 * 질문사항
-	 * 
-	 * startFunction(String menu){}라는 메서드를 만들고 검색, 수정, 삭제 기능을 합치는 방식.
-	 * 
-	 * 장점 - 코드 짧아짐, 한눈에 보기 좋음 단점 - 메서드 재사용 불가능해짐
-	 * 
-	 */
+// 		for (int i = 0; i < phoneAry.length; i++) {
+// 			System.out.printf("이름과 번호입력:");
+// 			String name = sc.next(), tel = sc.next();
+// 			phoneAry[i] = new Phone(name, tel);
+// 		}
+// 		System.out.println("저장되었습니다...");
+// 		System.out.println();
+// 	}
 
-	void showAllPhoneList() {
+// 	/*
+// 	 * 질문사항
+// 	 * 
+// 	 * startFunction(String menu){}라는 메서드를 만들고 검색, 수정, 삭제 기능을 합치는 방식.
+// 	 * 
+// 	 * 장점 - 코드 짧아짐, 한눈에 보기 좋음 단점 - 메서드 재사용 불가능해짐
+// 	 * 
+// 	 */
 
-		System.out.println("--------------------");
-		for (int i = 0; i < phoneAry.length; i++) {
-			if (phoneAry[i] != null) {
-				System.out.println(i + 1 + "." + phoneAry[i].getName() + " " + phoneAry[i].getTel());
-			}
-		}
-		System.out.println("--------------------");
+// 	void showAllPhoneList() {
 
-	}
+// 		System.out.println("--------------------");
+// 		for (int i = 0; i < phoneAry.length; i++) {
+// 			if (phoneAry[i] != null) {
+// 				System.out.println(i + 1 + "." + phoneAry[i].getName() + " " + phoneAry[i].getTel());
+// 			}
+// 		}
+// 		System.out.println("--------------------");
 
-	void searchPhone() {
-		System.out.printf("검색할 유저이름 입력:");
-		String userName = sc.next();
+// 	}
 
-		for (int i = 0; i < phoneAry.length; i++) {
-			if (userName.equals(phoneAry[i].getName())) {
-				System.out.println(userName + "의 전화번호는 " + phoneAry[i].getTel() + " 입니다.");
-				break;
-			}
-		}
+// 	void searchPhone() {
+// 		System.out.printf("검색할 유저이름 입력:");
+// 		String userName = sc.next();
 
-	}
+// 		for (int i = 0; i < phoneAry.length; i++) {
+// 			if (userName.equals(phoneAry[i].getName())) {
+// 				System.out.println(userName + "의 전화번호는 " + phoneAry[i].getTel() + " 입니다.");
+// 				break;
+// 			}
+// 		}
 
-	void editPhone() {
-		System.out.printf("수정할 유저이름 입력:");
-		String userName = sc.next();
+// 	}
 
-		for (int i = 0; i < phoneAry.length; i++) {
-			if (userName.equals(phoneAry[i].getName())) {
-				System.out.printf("이름과 번호입력:");
-				String name = sc.next(), tel = sc.next();
-				phoneAry[i] = new Phone(name, tel);
-				break;
-			}
-		}
-	}
+// 	void editPhone() {
+// 		System.out.printf("수정할 유저이름 입력:");
+// 		String userName = sc.next();
 
-	void deletePhone() {
-		System.out.printf("삭제할 유저이름 입력:");
-		String userName = sc.next();
+// 		for (int i = 0; i < phoneAry.length; i++) {
+// 			if (userName.equals(phoneAry[i].getName())) {
+// 				System.out.printf("이름과 번호입력:");
+// 				String name = sc.next(), tel = sc.next();
+// 				phoneAry[i] = new Phone(name, tel);
+// 				break;
+// 			}
+// 		}
+// 	}
 
-		for (int i = 0; i < phoneAry.length; i++) {
-			if (userName.equals(phoneAry[i].getName())) {
-				phoneAry[i] = null;
-				break;
-			}
-		}
-		showAllPhoneList();
-	}
+// 	void deletePhone() {
+// 		System.out.printf("삭제할 유저이름 입력:");
+// 		String userName = sc.next();
 
-	void run() {
-		initPhoneBook();
+// 		for (int i = 0; i < phoneAry.length; i++) {
+// 			if (userName.equals(phoneAry[i].getName())) {
+// 				phoneAry[i] = null;
+// 				break;
+// 			}
+// 		}
+// 		showAllPhoneList();
+// 	}
 
-		while (true) {
-			System.out.println("메뉴선택[전체검색,검색,삭제,수정,종료]");
-			System.out.printf("입력:");
-			String menu = sc.next();
+// 	void run() {
+// 		initPhoneBook();
 
-			switch (menu) {
+// 		while (true) {
+// 			System.out.println("메뉴선택[전체검색,검색,삭제,수정,종료]");
+// 			System.out.printf("입력:");
+// 			String menu = sc.next();
 
-			case SEARCH: {
-				searchPhone();
-				break;
-			}
-			case SHOWLIST: {
-				showAllPhoneList();
-				break;
-			}
-			case EDIT: {
-				editPhone();
-				break;
-			}
-			case DELETE: {
-				deletePhone();
-				break;
-			}
-			case EXIT: {
-				System.out.println("프로그램이 " + "종료되었습니다.");
-				return;
-			}
-			default:
-				throw new IllegalArgumentException("Unexpected value: " + menu);
-			}
-		}
+// 			switch (menu) {
 
-	}
+// 			case SEARCH: {
+// 				searchPhone();
+// 				break;
+// 			}
+// 			case SHOWLIST: {
+// 				showAllPhoneList();
+// 				break;
+// 			}
+// 			case EDIT: {
+// 				editPhone();
+// 				break;
+// 			}
+// 			case DELETE: {
+// 				deletePhone();
+// 				break;
+// 			}
+// 			case EXIT: {
+// 				System.out.println("프로그램이 " + "종료되었습니다.");
+// 				return;
+// 			}
+// 			default:
+// 				throw new IllegalArgumentException("Unexpected value: " + menu);
+// 			}
+// 		}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		new PhoneBook().run();
+// 	}
 
-	}
+// 	public static void main(String[] args) {
+// 		// TODO Auto-generated method stub
+// 		new PhoneBook().run();
 
-}
+// 	}
+
+// }
